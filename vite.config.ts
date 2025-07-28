4036a8bd820f3f0cb12fa5ad353eba720f2217ef
Ryan McCarty <23747348+ryankmccarty@users.noreply.github.com>
2025-07-28 11:16:54 -0400

    Configure Vite for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/snake/',
   plugins: [react()],
   build: {
+    outDir: 'docs',
     rollupOptions: {
       external: (id: string) => !id.startsWith('.') && !id.startsWith('/'),
     },
